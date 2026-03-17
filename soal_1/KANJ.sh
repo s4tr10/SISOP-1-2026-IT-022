6886575cfc48006058de2c2685ad39ce32502f5f
satwiwo27 <msatrioutomo2@gmail.com>
2026-03-17 21:16:10 +0700

    finish soal 1

---
+++ soal_1/KANJ.sh
@@ -0,0 +1,75 @@
+BEGIN {
+	FS = ","
+
+	opsi = ARGV[2]
+	delete ARGV[2]
+
+	if(opsi != "a" && opsi != "b" && opsi != "c" && opsi != "d" && opsi != "e") {
+		print("Soal tidak dikenali. Gunakan a, b, c, d, atau e.")
+		print("Contoh penggunaan: awk -f file.sh data.csv a")
+
+		exit 1
+	}
+
+	count_passenger = 0
+	oldest = 0
+	total_age = 0
+	total_business = 0
+}
+
+NR > 1 {
+	sub(/\r$/, "")
+
+	name = $1
+	age = $2
+	class = $3
+	carriage = $4
+
+	if(name != "") {
+		count_passenger++
+		total_age += age
+	}
+
+	if(carriage != ""){
+		list_carriage[carriage] = 1
+	}
+
+	if(age > oldest){
+		oldest = age
+		oldest_name = name
+	}
+
+	if(class ~ /Business/){
+		total_business++
+	}
+}
+
+END {
+	if(opsi == "a") {
+		print("Jumlah seluruh penumpang KANJ adalah", count_passenger, "orang.")
+	}
+
+	if(opsi == "b"){
+		total_carriage = 0
+		for(g in list_carriage){
+			total_carriage++
+		}
+
+		print("Jumlah gerbong penumpang KANJ adalah", total_carriage)
+	}
+
+	if(opsi == "c"){
+		print(oldest_name, "adalah penumpang kereta tertua dengan usia", oldest, "tahun.")
+	}
+
+	if(opsi == "d"){
+		average = total_age / count_passenger
+
+		printf("Rata-rata usia penumpang adalah %.0f tahun\n", average)
+	}
+
+	if(opsi == "e"){
+		print("Jumlah penumpang business class adalah", total_business, "orang")
+	}
+}
+
